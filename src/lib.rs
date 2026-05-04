45dafc3d9de03edccf83b1eccdc8ec4fc982c78e
solaren <zhouxiaolinux@163.com>
2026-05-04 17:01:50 +0800

    feat(text): add FontProvider trait + DefaultFontProvider

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub mod resource;
 pub mod runtime;
 pub mod scene;
 pub mod style;
-mod text;
+pub mod text;
 
 pub use frame_ctx::FrameCtx;
 pub use inspect::{FrameElementRect, collect_frame_layout_rects};
